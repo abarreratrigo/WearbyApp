320a51a94233dd1f6a71b3995e1ca9e7af858194
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-04-27 11:57:20 +0200

    Creada la clase Navegador - Se encarga de cambiar entre pantallas

--- wearbyApp/src/main/java/com/wearby/util/Navegador.java
+++ wearbyApp/src/main/java/com/wearby/util/Navegador.java
@@ -1,4 +1,43 @@
 package com.wearby.util;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Gestiona la navegación entre pantallas de la aplicación
+ * Centraliza la carga de archivos FXML para evitar repetición de código
+ */
 public class Navegador {
+
+    private static Stage stagePrincipal;
+
+    public static void setStagePrincipal(Stage stage){
+        stagePrincipal = stage;
+    }
+
+    public static void navegar(String rutaFxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                Navegador.class.getResource("/com/wearby/fxml/" + rutaFxml)
+        );
+
+        Parent root = loader.load();
+        stagePrincipal.getScene().setRoot(root);
+    }
+
+    public static <T> T navegarConControlador (String rutaFxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                Navegador.class.getResource("/com/wearby/fxml/" + rutaFxml)
+        );
+
+        Parent root = loader.load();
+        stagePrincipal.getScene().setRoot(root);
+        return loader.getController();
+    }
+
+    public static Stage getStage(){
+        return stagePrincipal;
+    }
 }
