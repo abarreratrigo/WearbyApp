57d4639cee2926b642cfab6540ce4da38f0c65b1
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-04-27 12:02:04 +0200

    Creada la clase Alertas - Se encarga de organizar los mensajes que se muestran en la aplicación (ERROR, INFO y CONFIRMACIÓN)

--- wearbyApp/src/main/java/com/wearby/util/Alertas.java
+++ wearbyApp/src/main/java/com/wearby/util/Alertas.java
@@ -1,4 +1,39 @@
 package com.wearby.util;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+/**
+ * Centraliza la creación de diálogos de alerta para mantener
+ * consistencia visual en toda la aplicación
+ */
+
 public class Alertas {
+
+    public static void error(String titulo, String mensaje){
+        Alert alert= new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static void info(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static boolean confirmar (String titulo, String mensaje){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        Optional<ButtonType> resultado = alert.showAndWait();
+        return resultado.isPresent() && resultado.get() == ButtonType.OK;
+    }
 }
