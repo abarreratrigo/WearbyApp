d6cff09a273889b4893b42f9a5700cd01486b703
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-04-27 11:50:00 +0200

    Creada la clase SesionUsuario - Se encarga de guardar los datos del usuario que ha iniciado sesión.

--- wearbyApp/src/main/java/com/wearby/sesion/SesionUsuario.java
+++ wearbyApp/src/main/java/com/wearby/sesion/SesionUsuario.java
@@ -1,4 +1,47 @@
 package com.wearby.sesion;
 
+/**
+ * Almacena los datos del usuario autenticado durante la sesión.
+ * Patrón Singleton: Una única estancia accesible globalmente
+ */
 public class SesionUsuario {
+
+    private static SesionUsuario instancia;
+
+    private Integer id;
+    private String nombre;
+    private String email;
+    private String rol;
+
+    private SesionUsuario(){}
+
+    public static SesionUsuario getInstancia() {
+        if (instancia == null){
+            instancia = new SesionUsuario();
+        }
+        return instancia;
+    }
+
+    public void iniciar(Integer id, String nombre, String email, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    public void cerrar() {
+        this.id = null;
+        this.nombre = null;
+        this.email = null;
+        this.rol = rol;
+    }
+
+    public boolean isAdmin(){
+        return "admin".equals(rol);
+    }
+
+    public Integer getId() {return id;}
+    public String getNombre() {return nombre;}
+    public String getEmail() {return email;}
+    public String getRol() {return rol;}
 }
