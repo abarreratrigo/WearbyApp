807b56645a98581d504b3f3f0b76bd552ab6886d
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-05-04 15:11:01 +0200

    Arreglado pequeño error al ejecutar

--- wearbyApp/src/main/java/module-info.java
+++ wearbyApp/src/main/java/module-info.java
@@ -1,15 +1,13 @@
 module wearbyApp {
-    requires com.google.gson;
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
+    requires com.google.gson;
     requires okhttp3;
+
     requires java.sql;
+
+    exports com.wearby to javafx.graphics;
 
     opens com.wearby to javafx.fxml;
     opens com.wearby.controlador to javafx.fxml;
-    opens com.wearby.modelo to javafx.fxml;
-
-    exports com.wearby;
-    opens com.wearby.dto to javafx.fxml;
 }
