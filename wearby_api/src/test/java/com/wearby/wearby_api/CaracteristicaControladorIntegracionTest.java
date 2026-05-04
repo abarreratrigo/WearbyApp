8230138327b3143aee9088b7abc538e02ae95d41
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-05-04 14:17:23 +0200

    Solucionado error en un test

--- wearby_api/src/test/java/com/wearby/wearby_api/CaracteristicaControladorIntegracionTest.java
+++ wearby_api/src/test/java/com/wearby/wearby_api/CaracteristicaControladorIntegracionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username = "test@wearby.com", roles = "USUARIO")
 public class CaracteristicaControladorIntegracionTest {
 
     @Autowired
