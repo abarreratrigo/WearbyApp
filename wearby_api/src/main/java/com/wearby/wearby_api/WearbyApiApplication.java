8eb93b82db4354e5137e83449b7b547b339a1096
abarreratrigo <abarreratrigo@gmail.com>
2026-05-14 20:56:36 +0200

    Instrucciones para la correcta ejecución en otro dispositivo

--- wearby_api/src/main/java/com/wearby/wearby_api/WearbyApiApplication.java
+++ wearby_api/src/main/java/com/wearby/wearby_api/WearbyApiApplication.java
@@ -3,6 +3,11 @@ package com.wearby.wearby_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Para poder iniciar la aplicación, ejecutad el sql
+ * en Workbench y modificad las application properties
+ * con vuestra contraseña del root
+ */
 @SpringBootApplication
 public class WearbyApiApplication {
 
